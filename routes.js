56bd330f5d7eff4abbd3df99a4c8e270f577df0c
Henry Myers <hankhowland@gmail.com>
2021-01-05 17:17:55 -0800

    sidebars now populating correctly

--- routes.js
+++ routes.js
@@ -29,6 +29,7 @@ router.get('/study', async function(req, res, next){
         var week_array = makeWeekArray();
         //get users study sessions
         const db = req.app.locals.db;
+
         var sessionsCursor = db.collection('studyTimes').find(
             {$and: [
                 {"date": {$gte: parseInt(mongoToday)}},
@@ -36,15 +37,31 @@ router.get('/study', async function(req, res, next){
             ]}   
         );
         var sessionsArray = [];
-
         await sessionsCursor.forEach((doc) => {
             doc.date = parseInt(doc.date.toString().slice(4,6)) + '/' + parseInt(doc.date.toString().slice(6,8)) + '/' + doc.date.toString().slice(0,4);
             doc.startTime = doc.startTime.toString().slice(0,5);
             doc.endTime = doc.endTime.toString().slice(0,5);
             sessionsArray.push(doc);
         });
+
+        var mySessionsCursor = db.collection('meetings').find({"studierEmail": {$eq: req.query.user}});
+        var mySessionsArray = [];
+        await mySessionsCursor.forEach((doc) => {
+            //find corresponding studyTime in sessionsArray
+            for (i in sessionsArray) {
+                var session = sessionsArray[i]
+                if (session._id == doc.studyTimeID) {
+                    doc.startTime = session.startTime;
+                    doc.endTime = session.endTime;
+                    doc.date = session.date;
+                }
+            }
+            if ((doc.date != undefined) && (dateStringToInt(doc.date) >= parseInt(mongoToday))) {
+                mySessionsArray.push(doc)
+            }
+        })
         //want to pass to client: date:"m/d", "start_time": "00:00", "end_time": "00:00", satisfied?: "True"/"False"
-        res.render('study', {daysArray: week_array, sessionsArr: sessionsArray, user: req.query.user});
+        res.render('study', {daysArray: week_array, sessionsArr: sessionsArray, user: req.query.user, myMeetings: mySessionsArray});
     }
 });
 
@@ -53,6 +70,8 @@ router.get('/motivate', async function(req, res, next){
     var week_array = makeWeekArray();
     //get all sessions where user != current user, date >= today, and motivator != 0
     const db = req.app.locals.db;
+
+    //available sessions
     var sessionsCursor = db.collection('studyTimes').find(
         {$and: [
             {"date": {$gte: parseInt(mongoToday)}},
@@ -69,10 +88,40 @@ router.get('/motivate', async function(req, res, next){
         var length = (parseInt(doc.endTime.split(':')[0])*60 + parseInt(doc.endTime.split(':')[1])) - (parseInt(doc.startTime.split(':')[0])*60 + parseInt(doc.startTime.split(':')[1])) 
         doc.cost = Math.ceil((length/60)*cost_per_hour);
         if (doc.cost < cost_per_hour) {doc.cost = cost_per_hour;}
-
         sessionsArray.push(doc);
     });
-    res.render('motivate', {sessions: sessionsArray, daysArray: week_array});
+
+    //my confirmed meetings
+    var mySessionsCursor = db.collection('meetings').find({"motivatorEmail": {$eq: req.query.user}});
+    var mySessionsArray = [];
+    var cursor = db.collection('studyTimes').find(
+        {$and: [
+            {"date": {$gte: parseInt(mongoToday)}},
+            {"email": {$ne: req.query.user}},
+        ]}   
+    );
+    var utilArray = [];
+    await cursor.forEach((doc) => {
+        doc.date = parseInt(doc.date.toString().slice(4,6)) + '/' + parseInt(doc.date.toString().slice(6,8)) + '/' + doc.date.toString().slice(0,4);
+        doc.startTime = doc.startTime.toString().slice(0,5);
+        doc.endTime = doc.endTime.toString().slice(0,5);
+        utilArray.push(doc);
+    });
+    await mySessionsCursor.forEach(async function(doc) {
+        for (i in utilArray) {
+            var sesh = utilArray[i];
+            if (sesh._id == doc.studyTimeID) {
+                sesh = utilArray[i];
+                doc.date = sesh.date;
+                doc.startTime = sesh.startTime;
+                doc.endTime = sesh.endTime;
+            }
+        }
+        if (doc.date != undefined) {
+            mySessionsArray.push(doc);
+        }
+    });
+    res.render('motivate', {sessions: sessionsArray, daysArray: week_array, myMeetings: mySessionsArray});
 });
 
 //add Study Session
@@ -223,4 +272,13 @@ function makeWeekArray() {
         week_array.push(new_day)
     }
     return week_array;
+}
+
+//m/d/yyyy string -> yyyymmdd int
+function dateStringToInt(date) {
+    var dateArr = date.split('/');
+    var month = zeroFormat(dateArr[0]);
+    var day = zeroFormat(dateArr[1]);
+    var year = dateArr[2];
+    return parseInt(`${year}${month}${day}`);
 }
