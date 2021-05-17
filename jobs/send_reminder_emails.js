606dc04b7e8dd0f36a780b5073b7a7dc1a533c59
Henry Myers <henrymyers@Henrys-MacBook-Pro.local>
2021-05-16 21:47:11 -0500

    reminder emails job

--- jobs/send_reminder_emails.js
+++ jobs/send_reminder_emails.js
@@ -1,4 +1,117 @@
-function work() {
-    console.log("hello from the send_reminder_emails job!!!");
+const mongo = require('mongodb');
+var nodemailer = require('nodemailer');
+
+function zeroFormat(hour) {
+    hour = hour.toString();
+    if (hour.length == 1) {
+        return "0" + hour.toString();
+    }
+    else {
+        return hour.toString();
+    }
 }
+
+var today = new Date();
+var mongoToday = today.getFullYear().toString()+zeroFormat(today.getMonth())+zeroFormat(today.getDate());
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    //replace with pollinate work email
+    auth: {
+        user: 'hankhowland@gmail.com',
+        pass: 'jojo3302'
+    }
+});
+
+
+function send_reminder_email(session, dest) {
+    var str = `Hey ${dest}, \n\n
+Reminder that you have a Pollinate Study Meeting today at ${timeToText(session.startTime)} CST!!
+Here is your meeting link:
+https://uchicago.zoom.us/j/96641521682?pwd=RFJqcmJvL285c3RSd3FHcmhpWnA1dz09\n
+
+Work is inevitable, procrastination doesn’t have to be.
+www.pollinate.work/routes/\n\n
+If you have any questions shoot us an email, text, or call.\n
+The Pollinate Team
+Jack Ogle: jackogle@uchicago.edu (612) 670-7721
+Jonathan Merril: jrmerril@uchicago.edu (703) 762-6410
+Henry Myers: hankhowland@gmail.com (971) 283-9172
+Joshua Weisskopf: jweisskopf@uchicago.edu (847) 721-2501\n
+P.S. Call us for all your procrastination needs or if you’re lonely!`
+
+    var mailOptions = {
+        from: 'hankhowland@gmail.com',
+        to: dest,
+        subject: 'Pollinate Meeting Reminder',
+        text: str
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+
+function work() {
+    //db connection url
+    const url = 'mongodb+srv://hankhowland:jojo3302@lists.9qsfv.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+    mongo.connect(url, { promiseLibrary: Promise }, async (err, client) => {
+        if (err) {
+            logger.warn(`Failed to connect to the database. ${err.stack}`);
+            return;
+        }
+
+        db = client.db('pollinate');
+        min_hour = today.getHours(); //current time
+        max_hour = today.getHours() + 2; //current time + 2 hours
+
+        var mySessionsArray = [];
+        mySessionsArray = await db.collection('meetings').find(
+            {$and: [
+                {"date": {$eq: parseInt(mongoToday)}}, //change to eq
+            ]}   
+        ).toArray();
+
+        console.log("today's sessions:");
+        console.log(mySessionsArray);
+
+        mySessionsArray.forEach((session) => {
+            startHour = session.startTime.split(':')[0];
+            if (startHour <= max_hour && startHour >= min_hour) {
+                console.log('sending reminder emails for session');
+                send_reminder_email(session, session.motivatorEmail);
+                send_reminder_email(session, session.studierEmail);
+            }
+        });
+        return;
+    }); 
+}
+
 work();
+
+
+
+
+//turn 00:00 -> x am/pm
+function timeToText(time) {
+    var timeArr = time.split(':');
+    var hour = timeArr[0];
+    var mins = ':' + timeArr[1].toString();
+    if (mins == ':00') {
+        mins='';
+    }
+    if (hour < 12) {
+        return `${parseInt(hour).toString()}${mins}am`;
+    }
+    else if (hour == 12) {
+        return `12${mins}pm`;
+    }
+    else {
+        normal_hour = parseInt(hour) - 12
+        return `${normal_hour.toString()}${mins}pm`;
+    }
+};
